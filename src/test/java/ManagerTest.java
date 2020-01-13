4c3e1a54379d66bce5a752476252b572c4f08c8e
Aresky Berkane <areskyberkane@gmail.com>
2020-01-13 14:15:25 +0000

    create develop class and test

---
+++ src/test/java/ManagerTest.java
@@ -0,0 +1,47 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.management.Manager;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class ManagerTest {
+    private Manager manager;
+
+    @Before
+    public void before(){
+        manager = new Manager("Juan",495,2045.89,"CodeClan");
+    }
+
+    @Test
+    public void shouldReturnName() {
+        assertEquals("Juan",manager.getName());
+    }
+
+    @Test
+    public void shouldRaiseSalary() {
+        manager.raiseSalary(1);
+        assertEquals(2046.89,manager.getSalary(),0.01);
+    }
+
+    @Test
+    public void shouldGetPayBonusAmount() {
+        double expectedBonusAmount = manager.getSalary() * 0.01;
+        assertEquals(expectedBonusAmount, manager.payBonus(),0.01);
+    }
+
+    @Test
+    public void shouldReturnDeptName() {
+        assertEquals("CodeClan",manager.getDeptName());
+    }
+
+    @Test
+    public void shouldReturnNiNumber() {
+        assertEquals(495,manager.getNiNumber());
+    }
+
+    @Test
+    public void shouldReturnSalary() {
+        assertEquals(2045.89,manager.getSalary(),0.01);
+    }
+}
